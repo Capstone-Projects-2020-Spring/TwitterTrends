d160cd9138e236dc9098fd43039af54c59c7f290
BareBeaverBat <ssiliss72@gmail.com>
2020-02-21 14:41:54 -0500

    add trivial automated test and verify selenium is working

--- WebAppAutoTesting/src/main/java/AutoTestProto.java
+++ WebAppAutoTesting/src/main/java/AutoTestProto.java
@@ -1,16 +1,44 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.nio.file.FileSystems;
 
 public class AutoTestProto
 {
 
+	@Test
 	public void seleniumTest( )
 	{
-		String relativeChromeDriverPath = "/src/main/chromedriver.exe";
+		String relativeChromeDriverPath = "src/main/chromedriver.exe";
 		String absoluteChromeDriverPath = FileSystems.getDefault()
 													 .getPath(relativeChromeDriverPath)
 													 .normalize()
 													 .toAbsolutePath()
 													 .toString();
 		System.setProperty("webdriver.chrome.driver", absoluteChromeDriverPath);
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com/");
+		testPause();
+		WebElement searchBox = driver.findElement(By.name("q"));
+		searchBox.sendKeys("ChromeDriver");
+		searchBox.submit();
+		testPause();
+		driver.quit();
+	}
+
+	private void testPause( )
+	{
+		try
+		{
+			Thread.sleep(3000);
+		}
+		catch ( InterruptedException e )
+		{
+			e.printStackTrace();
+		}
 	}
 }
