1bd48e785339aa20cb85345621301562c27a14f6
Thanh Tran <tug73060@temple.edu>
2020-02-23 22:19:53 -0500

    some more HTML

--- twitterapp/routes.py
+++ twitterapp/routes.py
@@ -1,15 +1,22 @@
 from flask import render_template
 from twitterapp import app
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html')
 
 
+@app.route("/trend")
+def trend():
+    return render_template('trend.html')
+
+
 @app.route("/user")
 def user():
     return render_template('user.html')
+
 
 
 @app.route("/login")
