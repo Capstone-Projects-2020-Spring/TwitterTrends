1b24e8edf8e76ff198d2036df738b34fd90610e7
Thanh Tran <tug73060@temple.edu>
2020-02-23 21:40:50 -0500

    add skeleton with HTML/CSS empty

--- twitterapp/routes.py
+++ twitterapp/routes.py
@@ -2,5 +2,17 @@ from flask import render_template
 from twitterapp import app
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return 'Home Page of Twitter Trends'
+    return render_template('home.html')
+
+
+@app.route("/user")
+def user():
+    return render_template('user.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
