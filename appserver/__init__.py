2c323f8c62e4dedf7088efb6f29e506439e11d95
Thanh Tran <tug73060@temple.edu>
2020-02-29 15:50:41 -0500

    moved all bootstrap inside static folder + rendered html

--- appserver/__init__.py
+++ appserver/__init__.py
@@ -4,6 +4,5 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
