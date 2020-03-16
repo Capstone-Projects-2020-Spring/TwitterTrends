109e2ee196e1b2763b6728539d531876466a7cf2
Cnelson10 <c.lawsonnelson@gmail.com>
2020-03-16 14:11:50 -0400

    remove links from trends

--- appserver/website-bootstrap/main.js
+++ appserver/website-bootstrap/main.js
@@ -7,10 +7,14 @@ function retrieveTrends(trendUrl) {
 	$.getJSON(trendUrl, function(data){
 		trends = data;
 	}).then(function() {
-	    document.querySelector('.trend-1').innerHTML = trends[0].trend_content;
-	    document.querySelector('.trend-2').innerHTML = trends[1].trend_content;
-	    document.querySelector('.trend-3').innerHTML = trends[2].trend_content;
-	    document.querySelector('.trend-4').innerHTML = trends[3].trend_content;
-	    document.querySelector('.trend-5').innerHTML = trends[4].trend_content;
+	    document.querySelector('.trend-1').innerHTML = "1. " + trends[0].trend_content;
+	    document.querySelector('.trend-2').innerHTML = "2. " + trends[1].trend_content;
+	    document.querySelector('.trend-3').innerHTML = "3. " + trends[2].trend_content;
+	    document.querySelector('.trend-4').innerHTML = "4. " + trends[3].trend_content;
+	    document.querySelector('.trend-5').innerHTML = "5. " + trends[4].trend_content;
 	});
+}
+
+function getMoreInfo() {
+
 }
