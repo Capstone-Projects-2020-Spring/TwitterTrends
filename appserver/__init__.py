06499f02652187701e81349e856a723bce69f3c5
Thanh Tran <tug73060@temple.edu>
2020-03-15 23:03:25 -0400

    redirect paths in html

--- appserver/__init__.py
+++ appserver/__init__.py
@@ -6,3 +6,18 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('index.html')
+
+
+@app.route("/location")
+def location():
+    return render_template('location.html')
+
+
+@app.route("/network")
+def network():
+    return render_template('network.html')
+
+
+@app.route("/time")
+def time():
+    return render_template('time.html')
