0f285605952432d10f40e0c17c27e03e66ff44bd
Dan Tran <tug06533@temple.edu>
2020-03-19 14:43:24 -0400

    added class template for temporal data retrieval manager

---
+++ Backend/TemporalDataManager.py
@@ -0,0 +1,19 @@
+import threading
+
+
+class TemporalDataManager:
+
+    # TODO: set up instance of algorithm manager (and anything else needed)
+    # TODO: tasked to set up threads that can access trends data retrieving functions
+    def __init__(self):
+        ()
+
+    # TODO: this method will be periodically called by a thread
+    # TODO: tasked to retrieve trends data and store them somewhere
+    def periodic_trends_retrieval(self):
+        ()
+
+    # TODO: periodically pull tweets at different dates
+    # TODO: this method template is an oversight and it might not be necessary
+    def periodic_tweets_retrieval(self):
+        ()
