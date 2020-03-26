bac7312131814e08197a806047be3001656d8832
BareBeaverBat <ssiliss72@gmail.com>
2020-03-26 17:43:44 -0400

    modify pojos to use lombok

--- WebAppAutoTesting/src/main/java/api/responses/NewsStory.java
+++ WebAppAutoTesting/src/main/java/api/responses/NewsStory.java
@@ -1,8 +1,13 @@
 package api.responses;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * stores information about a new story (which is related to some trend) sent from the backend server
  */
+@Data
+@NoArgsConstructor
 public class NewsStory
 {
 protected String title;
@@ -12,88 +17,4 @@ protected String linkUrl;
 protected String date;
 protected String description;
 protected String contentStart;
-
-public NewsStory( ) {}
-
-public NewsStory( final String title, final String author, final String source_name, final String link_url,
-	final String date, final String description, final String content_start )
-{
-	this.title = title;
-	this.author = author;
-	this.sourceName = source_name;
-	this.linkUrl = link_url;
-	this.date = date;
-	this.description = description;
-	this.contentStart = content_start;
-}
-
-public String getTitle( )
-{
-	return title;
-}
-
-public void setTitle( final String title )
-{
-	this.title = title;
-}
-
-public String getAuthor( )
-{
-	return author;
-}
-
-public void setAuthor( final String author )
-{
-	this.author = author;
-}
-
-public String getSourceName( )
-{
-	return sourceName;
-}
-
-public void setSourceName( final String sourceName )
-{
-	this.sourceName = sourceName;
-}
-
-public String getLinkUrl( )
-{
-	return linkUrl;
-}
-
-public void setLinkUrl( final String linkUrl )
-{
-	this.linkUrl = linkUrl;
-}
-
-public String getDate( )
-{
-	return date;
-}
-
-public void setDate( final String date )
-{
-	this.date = date;
-}
-
-public String getDescription( )
-{
-	return description;
-}
-
-public void setDescription( final String description )
-{
-	this.description = description;
-}
-
-public String getContentStart( )
-{
-	return contentStart;
-}
-
-public void setContentStart( final String contentStart )
-{
-	this.contentStart = contentStart;
-}
 }
--- WebAppAutoTesting/src/main/java/api/responses/Trend.java
+++ WebAppAutoTesting/src/main/java/api/responses/Trend.java
@@ -1,10 +1,14 @@
 package api.responses;
 
 import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * stores information about a trend sent from the backend server
  */
+@NoArgsConstructor
+@Data
 public class Trend
 {
 @SerializedName(value = "trend_PK")
@@ -14,66 +18,4 @@ protected String trendContent;
 protected boolean isHashtag;
 protected String queryTerm;
 protected int tweetVolume;
-
-public Trend( ) {}
-
-public Trend( final int trendPk, final String trendContent, final boolean isHashtag, final String queryTerm,
-	final int tweetVolume )
-{
-	this.trendPk = trendPk;
-	this.trendContent = trendContent;
-	this.isHashtag = isHashtag;
-	this.queryTerm = queryTerm;
-	this.tweetVolume = tweetVolume;
-}
-
-public int getTrendPk( )
-{
-	return trendPk;
-}
-
-public void setTrendPk( final int trendPk )
-{
-	this.trendPk = trendPk;
-}
-
-public String getTrendContent( )
-{
-	return trendContent;
-}
-
-public void setTrendContent( final String trendContent )
-{
-	this.trendContent = trendContent;
-}
-
-public boolean isHashtag( )
-{
-	return isHashtag;
-}
-
-public void setHashtag( final boolean hashtag )
-{
-	isHashtag = hashtag;
-}
-
-public String getQueryTerm( )
-{
-	return queryTerm;
-}
-
-public void setQueryTerm( final String queryTerm )
-{
-	this.queryTerm = queryTerm;
-}
-
-public int getTweetVolume( )
-{
-	return tweetVolume;
-}
-
-public void setTweetVolume( final int tweetVolume )
-{
-	this.tweetVolume = tweetVolume;
-}
 }
