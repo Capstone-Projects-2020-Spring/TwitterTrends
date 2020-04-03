a7899517343c7e4a0106dac1de41a8a3699dada4
BareBeaverBat <ssiliss72@gmail.com>
2020-04-03 14:32:04 -0400

    streamline happy path location test code and cover remaining arguments

--- WebAppAutoTesting/src/test/java/api/GetLocationApiTests.java
+++ WebAppAutoTesting/src/test/java/api/GetLocationApiTests.java
@@ -28,63 +28,127 @@ protected static final double LAT_LONG_COMPARISON_THRESHOLD = 0.5;
 @Override
 protected String getEndpoint( ) { return "getlocation"; }
 
+//HAPPY PATH TESTS
+
 @Test
-public void getGreensboroByAddressTest( )
+public void getGreensboroByAddressTest( ) { testLocByAddr(Locations.GREENSBORO); }
+
+@Test
+public void getSaltLakeCityByAddressTest( ) { testLocByAddr(Locations.SALT_LAKE_CITY); }
+
+@Test
+public void getOmahaByAddressTest( ) { testLocByAddr(Locations.OMAHA); }
+
+@Test
+public void getRaleighByAddressTest( ) { testLocByAddr(Locations.RALEIGH); }
+
+@Test
+public void getNewYorkByAddressTest( ) { testLocByAddr(Locations.NEW_YORK); }
+
+@Test
+public void getSanFranciscoByAddressTest( ) { testLocByAddr(Locations.SAN_FRANCISCO); }
+
+@Test
+public void getGreensboroByWoeidTest( ) { testLocByWoeid(Locations.GREENSBORO); }
+
+@Test
+public void getSaltLakeCityByWoeidTest( ) { testLocByWoeid(Locations.SALT_LAKE_CITY); }
+
+@Test
+public void getOmahaByWoeidTest( ) { testLocByWoeid(Locations.OMAHA); }
+
+@Test
+public void getRaleighByWoeidTest( ) { testLocByWoeid(Locations.RALEIGH); }
+
+@Test
+public void getNewYorkByWoeidTest( ) { testLocByWoeid(Locations.NEW_YORK); }
+
+@Test
+public void getSanFranciscoByWoeidTest( ) { testLocByWoeid(Locations.SAN_FRANCISCO); }
+
+@Test
+public void getGreensboroByLatLongTest( ) { testLocByLatLong(Locations.GREENSBORO); }
+
+@Test
+public void getSaltLakeCityByLatLongTest( ) { testLocByLatLong(Locations.SALT_LAKE_CITY); }
+
+@Test
+public void getOmahaByLatLongTest( ) { testLocByLatLong(Locations.OMAHA); }
+
+@Test
+public void getRaleighByLatLongTest( ) { testLocByLatLong(Locations.RALEIGH); }
+
+@Test
+public void getNewYorkByLatLongTest( ) { testLocByLatLong(Locations.NEW_YORK); }
+
+@Test
+public void getSanFranciscoByLatLongTest( ) { testLocByLatLong(Locations.SAN_FRANCISCO); }
+
+//NEGATIVE TESTS todo
+
+// TEST HELPER FUNCTIONS
+
+/**
+ * tests the api endpoint for a particular location by sending a vague address string for the city
+ *
+ * @param testLoc the location to be tested
+ */
+protected void testLocByAddr( Locations testLoc )
 {
-	Locations testLoc = Locations.GREENSBORO;
 	Response response = given().param(ADDR_ARG, buildLocationAddrString(testLoc))
 							   .get();
 	Location location = parseLocation(response);
 	validateLocation(location, testLoc);
 }
 
-@Test
-public void getSaltLakeCityByAddressTest( )
+/**
+ * tests the api endpoint for a particular location by sending the city's woeid
+ *
+ * @param testLoc the location to be tested
+ */
+protected void testLocByWoeid( Locations testLoc )
 {
-	Locations testLoc = Locations.SALT_LAKE_CITY;
-	Response response = given().param(ADDR_ARG, buildLocationAddrString(testLoc))
+	Response response = given().param(WOEID_ARG, testLoc.getWoeidStr())
 							   .get();
 	Location location = parseLocation(response);
 	validateLocation(location, testLoc);
 }
 
-@Test
-public void getOmahaByAddressTest( )
+/**
+ * tests the api endpoint for a particular location by sending the city's latitude and longitude
+ *
+ * tests both ways of sending either parameter to the endpoint
+ *
+ * @param testLoc the location to be tested
+ */
+protected void testLocByLatLong( Locations testLoc )
 {
-	Locations testLoc = Locations.OMAHA;
-	Response response = given().param(ADDR_ARG, buildLocationAddrString(testLoc))
+	//full names - latitude & longitude
+	Response response = given().param(LAT_ARG1, testLoc.getLatitude())
+							   .param(LONG_ARG1, testLoc.getLongitude())
 							   .get();
 	Location location = parseLocation(response);
 	validateLocation(location, testLoc);
-}
-
-@Test
-public void getRaleighByAddressTest( )
-{
-	Locations testLoc = Locations.RALEIGH;
-	Response response = given().param(ADDR_ARG, buildLocationAddrString(testLoc))
-							   .get();
-	Location location = parseLocation(response);
+	
+	//full and short names - latitude & long
+	response = given().param(LAT_ARG1, testLoc.getLatitude())
+					  .param(LONG_ARG2, testLoc.getLongitude())
+					  .get();
+	location = parseLocation(response);
 	validateLocation(location, testLoc);
-}
-
-@Test
-public void getNewYorkByAddressTest( )
-{
-	Locations testLoc = Locations.NEW_YORK;
-	Response response = given().param(ADDR_ARG, buildLocationAddrString(testLoc))
-							   .get();
-	Location location = parseLocation(response);
+	
+	//short full names - lat & longitude
+	response = given().param(LAT_ARG2, testLoc.getLatitude())
+					  .param(LONG_ARG1, testLoc.getLongitude())
+					  .get();
+	location = parseLocation(response);
 	validateLocation(location, testLoc);
-}
-
-@Test
-public void getSanFranciscoByAddressTest( )
-{
-	Locations testLoc = Locations.SAN_FRANCISCO;
-	Response response = given().param(ADDR_ARG, buildLocationAddrString(testLoc))
-							   .get();
-	Location location = parseLocation(response);
+	
+	//short names - lat & long
+	response = given().param(LAT_ARG2, testLoc.getLatitude())
+					  .param(LONG_ARG2, testLoc.getLongitude())
+					  .get();
+	location = parseLocation(response);
 	validateLocation(location, testLoc);
 }
 
@@ -94,7 +158,7 @@ public void getSanFranciscoByAddressTest( )
  * @param locPojo  the pojo parsed from an api response
  * @param location the enum entry describing what the location's values actually are
  */
-public void validateLocation( Location locPojo, Locations location )
+protected void validateLocation( Location locPojo, Locations location )
 {
 	assertNotNull(locPojo);
 	assertEquals(location.getCityName(), locPojo.getCityId());
