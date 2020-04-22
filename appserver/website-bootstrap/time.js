d1517dc95b66f4fdbc08c9c7036a6750cfa3fa7f
Mike Lauw <tug01761@temple.edu>
2020-04-22 12:25:05 -0400

    changing ID name for the following: search6->location-search-box

--- appserver/website-bootstrap/time.js
+++ appserver/website-bootstrap/time.js
@@ -469,6 +469,6 @@ function autocomplete(inp, arr) {
 /*An array containing all the country names in the world:*/
 var cities = ["World","United States","New York","Los Angeles","Chicago","Houston","Phoenix","Philadelphia","San Antonio","Dallas","San Diego","San Jose","Detroit", "San Francisco","Jacksonville","Indianapolis","Austin","Columbus","Charlotte","Memphis","Baltimore","Boston","El Paso","Milwaukee","Denver","Seattle","Nashville","Washington","Las Vegas","Portland","Louisville","Oklahoma City","Tucson","Atlanta","Albuquerque","Kansas City","Fresno","Sacramento","Long Beach","Mesa","Omaha","Cleveland","Virginia Beach","Miami","Raleigh","Minneapolis","Colorado Springs","Honolulu","St. Louis","Tampa","New Orleans","Cincinnati","Pittsburgh","Greensboro","Norfolk","Orlando","Birmingham","Baton Rouge","Richmond","Salt Lake City","Jackson","Tallahassee","Providence","New Haven","Harrisburg"];
 
-autocomplete(document.getElementById("search6"), cities);
+autocomplete(document.getElementById("location-search-box"), cities);
 }
 
